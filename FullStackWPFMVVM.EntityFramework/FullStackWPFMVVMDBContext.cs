78ecd5d0258ba532d4fe695e9c5a28fc214356c8
Aaron Parrilla <aaronparrilla112@outlook.com>
2020-11-09 11:13:45 +0100

    Add project files.

---
+++ FullStackWPFMVVM.EntityFramework/FullStackWPFMVVMDBContext.cs
@@ -0,0 +1,22 @@
+﻿using FullStackWPFMVVM.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FullStackWPFMVVM.EntityFramework
+{
+    public class FullStackWPFMVVMDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AssetTransaction> AssetTransactions { get; set; }
+        public FullStackWPFMVVMDBContext(DbContextOptions options) : base(options){ }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AssetTransaction>().OwnsOne(a => a.Stock);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ FullStackWPFMVVM.EntityFramework/FullStackWPFMVVMDBContextOptionsFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FullStackWPFMVVM.EntityFramework
+{
+    public class FullStackWPFMVVMDBContextOptionsFactory : IDesignTimeDbContextFactory<FullStackWPFMVVMDBContext>
+    {
+        public FullStackWPFMVVMDBContext CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<FullStackWPFMVVMDBContext>();
+            options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=FullStackWPFMVVMDB;Trusted_Connection=True;");
+            return new FullStackWPFMVVMDBContext(options.Options);
+        }
+    }
+}
